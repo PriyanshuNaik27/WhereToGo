eb85a441327465fec43bb29c46df02317ca1e82e
PriyanshuNaik27 <priyanshunaik143@gmail.com>
2025-07-14 16:34:18 +0530

    added zod

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,12 +1,49 @@
-import { asyncHandler } from  "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { z } from "zod";
+const registerUser = asyncHandler(async (req, res) => {
+  //get user details from frontend by req.body
+  // req.body -> username ,email, password;
+  //validation ->not empty
+  //check if user already exists
+  //create user object ;->creaate entry in db
+  //remove password and refresh token from response
+  //check for user creation
+  //return res
 
-const registerUser = asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message:"ok"
-    })
-})
+  // console.log(userName,email);
+  // res.json({
+  //     "gotEmail": email,
+  // }
 
+  //validation using some  ->but it is only for empty or not
 
+  // if(
+  //     [userName,email,password].some((field)=>{
+  //         return  field?.trim()==="";
+  //     })
+  // ){
+  //     throw new ApiError(400,"Some of the field is empty");
+  // }
 
+  //zod validation
 
-export {registerUser}
+  const requireBody = z.object({
+    email: z.email().min(3).max(100),
+    userName: z.string().min(1).max(50),
+    password: z.string().min(1).max(50),
+  });
+
+  const parsedDataWithSucces = requireBody.safeParse(req.body);
+
+  if (!parsedDataWithSucces.success) {
+    throw new ApiError(400, "Invalid input fields", parsedDataWithSucces.error);
+  }
+  const { userName, email, password } = req.body;
+  console.log(userName, email);
+  res.json({
+    gotEmail: email,
+  });
+});
+
+export { registerUser };
